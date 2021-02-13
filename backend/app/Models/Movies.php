335c770cab6bec02c565f6fe83bd5dc09d13a97d
fufapadurii <=>
2021-02-13 12:47:11 +0200

    added fillable property

--- backend/app/Models/Movies.php
+++ backend/app/Models/Movies.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movies extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'rating', 'description', 'image'];
 
     public function artists(): BelongsToMany
     {
