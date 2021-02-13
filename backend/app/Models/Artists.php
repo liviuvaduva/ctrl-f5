6834b6b2d687839f8dbbbd2f796dc57001857f4f
fufapadurii <=>
2021-02-13 11:30:12 +0200

    add artist to movies and vice-verse relationship

--- backend/app/Models/Artists.php
+++ backend/app/Models/Artists.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Artists extends Model
 {
     use HasFactory;
+
+    /**
+     * @return BelongsToMany
+     */
+    public function movies()
+    {
+        return $this->belongsToMany(Movies::class);
+    }
 }
--- backend/app/Models/Movies.php
+++ backend/app/Models/Movies.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movies extends Model
 {
     use HasFactory;
+
+    public function artists(): BelongsToMany
+    {
+        return $this->belongsToMany(Artists::class);
+    }
+
 }
