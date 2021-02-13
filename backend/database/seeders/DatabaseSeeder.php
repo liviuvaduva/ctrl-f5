512587b46cda56c76b1fb62a623371e71c58aa37
fufapadurii <=>
2021-02-13 11:30:47 +0200

    seed database using the movies as starting point

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        Artists::factory(10)->create();
-        Movies::factory(10)->create();
+        Movies::factory(10)->create()->each(function ($movie) {
+            $artists = Artists::factory(10)->create();
+            $movie->artists()->saveMany($artists);
+        });
     }
 }
